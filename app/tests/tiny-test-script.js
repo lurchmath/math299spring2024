a07341dfcd454edd7c13d3622759e9925527b76e
Nathan Carter <ncarter@bentley.edu>
2024-07-29 09:57:41 -0400

    Delete dev tools to minimize repo contents

--- app/tests/tiny-test-script.js
+++
@@ -1,5 +0,0 @@
-
-// This file can be imported using the loadScript() function defined in
-// utilities.js as part of the testing of that function, to ensure it works.
-// See utilities-test.js.
-window.globalValueForTesting = 5
