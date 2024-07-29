6bd5c248b5a05a66942580234aeaf52acb4b1634
Nathan Carter <ncarter@bentley.edu>
2024-07-29 10:00:24 -0400

    Ensure no undesired upstream changes come in later

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,3 +1,10 @@
 
 # Install all node modules
 npm install
+
+# When this fork receives changes from upstream, we want it to ignore any
+# changes we made in this fork.  The reason is because this fork deletes many of
+# the development tools in the upstream repo, to make this repo minimalist and
+# small.  We don't want those tools added back in later.  See the .gitattributes
+# file for details.
+git config --global merge.ours.driver true
